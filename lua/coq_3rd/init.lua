a670630bcac3fdf5675f91048b64b8dd3cd1d3b5
Fly me to the moon <git@bigly.dog>
2021-09-13 10:46:48 -0700

    uids

--- lua/coq_3rd/init.lua
+++ lua/coq_3rd/init.lua
@@ -1,3 +1,14 @@
+local uid = function(sources)
+  key = nil
+  while true do
+    if not key or sources[key] then
+      key = math.floor(math.random() * 10000)
+    else
+      return key
+    end
+  end
+end
+
 return function(sources)
   COQsources = COQsources or {}
 
@@ -5,7 +16,7 @@ return function(sources)
     if type(spec.src) == "string" then
       local init = require("coq_3rd." .. spec.src)
       spec.short_name = spec.short_name or string.upper(spec.src)
-      COQsources[vim.fn.tempname()] = {name = spec.short_name, fn = init(spec)}
+      COQsources[uid(COQsources)] = {name = spec.short_name, fn = init(spec)}
     end
   end
 end
