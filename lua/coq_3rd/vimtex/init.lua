9d23f8d19d1b37e561c60bf2b0e6516a1e9598cc
Fly me to the moon <git@bigly.dog>
2021-09-13 11:28:12 -0700

    vimte

--- lua/coq_3rd/vimtex/init.lua
+++ lua/coq_3rd/vimtex/init.lua
@@ -2,8 +2,22 @@ local trans = require("coq_3rd.trans").completefunc
 
 return function(spec)
   return function(args, callback)
-    local matches =
-      vim.fn["vimtex#complete#omnifunc"](0, vim.fn.expand("<cword>"))
-    callback(trans(matches))
+    local row, col = unpack(args.pos)
+
+    local pos = vim.fn["vimtex#complete#omnifunc"](1, "")
+    if pos == -2 or pos == -3 then
+      callback(nil)
+    else
+      local cword = (function()
+        if pos < 0 or pos >= col then
+          return vim.fn.expand("<cword>")
+        else
+          return vim.fn.expand("<cword>")
+        end
+      end)()
+      local matches = vim.fn["vimtex#complete#omnifunc"](0, cword)
+      local words = matches.words and matches.words or matches
+      callback {isIncomplete = true, items = trans(words)}
+    end
   end
 end
