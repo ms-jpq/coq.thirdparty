bc8d62925940e13608d70829494c8382fa4522c0
Fly me to the moon <git@bigly.dog>
2021-09-17 07:32:22 -0700

    ok

--- lua/cmp/inverse.lua
+++ lua/cmp/inverse.lua
@@ -18,7 +18,11 @@ return function()
     function src.get_trigger_characters()
       return {}
     end
-    function src.complete(_, args, callback)
+    function src.complete(_, cmp_args, callback)
+      local args = {
+        pos = {cmp_args.context.cursor.line,cmp_args.context.cursor.col},
+        line = cmp_args.context.cursor_line
+      }
       val(args, callback)
     end
     function src.resolve(_, item, callback)
