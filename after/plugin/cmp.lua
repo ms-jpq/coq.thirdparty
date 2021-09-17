fe7b3231dc56d97423e0cbca39436473d752a71a
Fly me to the moon <git@bigly.dog>
2021-09-17 02:37:14 -0700

    fmt

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,5 +1,7 @@
 local utils = require("coq_3p.utils")
 
+local M = {}
+
 local trans =
   (function()
   local session_id = -1
@@ -21,42 +23,44 @@ local trans =
   end
 end)()
 
-return {
-  register_source = function(name, cmp_source)
-    COQsources = COQsources or {}
-    vim.validate {
-      COQsources = {COQsources, "table"}
-    }
+M.register_source = function(name, cmp_source)
+  COQsources = COQsources or {}
+  vim.validate {
+    COQsources = {COQsources, "table"}
+  }
 
-    local cont = function()
-      vim.validate {cmp_source = {cmp_source, "table"}}
-      local triggers = cmp_source:get_trigger_characters()
-      for idx, char in ipairs(triggers) do
-        vim.validate {idx = {idx, "number"}, char = {char, "string"}}
-      end
-
-      local go = function(before_cursor)
-        for _, char in ipairs(triggers) do
-          if vim.endswith(before_cursor, char) then
-            return true
-          end
-        end
-        return false
-      end
-
-      local coq_source = function(args, callback)
-        if go(lhs) then
-          callback(nil)
-        else
-          local cmp_args = trans(args)
-          cmp_source:complete(cmp_args, callback)
-        end
-      end
+  local cont = function()
+    vim.validate {cmp_source = {cmp_source, "table"}}
+    local triggers = cmp_source:get_trigger_characters()
+    for idx, char in ipairs(triggers) do
+      vim.validate {idx = {idx, "number"}, char = {char, "string"}}
     end
 
-    local go, err = pcall(cont)
-    if not go then
-      vim.api.nvim_err_writeln(err)
+    local go = function(before_cursor)
+      for _, char in ipairs(triggers) do
+        if vim.endswith(before_cursor, char) then
+          return true
+        end
+      end
+      return false
+    end
+
+    local coq_source = function(args, callback)
+      if go(lhs) then
+        callback(nil)
+      else
+        local cmp_args = trans(args)
+        cmp_source:complete(cmp_args, callback)
+      end
     end
   end
-}
+
+  local go, err = pcall(cont)
+  if not go then
+    vim.api.nvim_err_writeln(err)
+  end
+end
+
+M.lsp = vim.lsp.protocol
+
+return M
