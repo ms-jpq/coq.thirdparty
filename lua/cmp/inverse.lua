f5c12e1a06ca79526ce7e78d9b60ce5bfb5f5a50
Fly me to the moon <git@bigly.dog>
2021-09-22 09:23:09 -0700

    added warn on err

--- lua/cmp/inverse.lua
+++ lua/cmp/inverse.lua
@@ -4,54 +4,59 @@ return function()
     COQsources = {COQsources, "table"}
   }
 
+  local utils = require("coq_3p.utils")
+
   local trans = function(key, val)
     vim.validate {
       val = {val, "function"}
     }
 
-    local src = {}
-    function src.is_available()
-      return true
-    end
-    function src.get_debug_name()
-      return "-- BRIDGED coq.nvim --\t" .. tostring(key)
-    end
-    function src.get_keyword_pattern()
-      return "\v.$"
-    end
-    function src.get_trigger_characters()
-      return {}
-    end
-    function src.complete(_, cmp_args, callback)
-      vim.validate {
-        args = {cmp_args, "table"}
+    return utils.freeze(
+      "coq_3p.inverse_src",
+      {
+        is_available = function()
+          return true
+        end,
+        get_debug_name = function()
+          return "-- BRIDGED coq.nvim --\t" .. tostring(key)
+        end,
+        get_keyword_pattern = function()
+          return "\v.$"
+        end,
+        get_trigger_characters = function()
+          return {}
+        end,
+        complete = function(_, cmp_args, callback)
+          vim.validate {
+            args = {cmp_args, "table"}
+          }
+          vim.validate {
+            time = {cmp_args.time, "number"},
+            context = {cmp_args.context, "table"}
+          }
+          vim.validate {
+            cursor = {cmp_args.context.cursor, "table"},
+            line = {cmp_args.context.cursor_line, "string"}
+          }
+          vim.validate {
+            row = {cmp_args.context.cursor.line, "number"},
+            col = {cmp_args.context.cursor.col, "number"}
+          }
+          local args = {
+            uid = cmp_args.time,
+            pos = {cmp_args.context.cursor.line, cmp_args.context.cursor.col},
+            line = cmp_args.context.cursor_line
+          }
+          val(args, callback)
+        end,
+        resolve = function(_, _, callback)
+          callback(nil)
+        end,
+        execute = function(_, _, callback)
+          callback(nil)
+        end
       }
-      vim.validate {
-        time = {cmp_args.time, "number"},
-        context = {cmp_args.context, "table"}
-      }
-      vim.validate {
-        cursor = {cmp_args.context.cursor, "table"},
-        line = {cmp_args.context.cursor_line, "string"}
-      }
-      vim.validate {
-        row = {cmp_args.context.cursor.line, "table"},
-        col = {cmp_args.context.cursor.col, "table"}
-      }
-      local args = {
-        uid = cmp_args.time,
-        pos = {cmp_args.context.cursor.line, cmp_args.context.cursor.col},
-        line = cmp_args.context.cursor_line
-      }
-      val(args, callback)
-    end
-    function src.resolve(_, item, callback)
-      callback(nil)
-    end
-    function src.execute(_, item, callback)
-      callback(nil)
-    end
-    return src
+    )
   end
 
   local acc = {}
