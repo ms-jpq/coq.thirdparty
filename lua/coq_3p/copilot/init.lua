8487889f16834c5b3815fc1fee0be3dc1c988655
root <root@dev.lan>
2021-10-28 17:46:06 -0700

    rm tabfallback

--- lua/coq_3p/copilot/init.lua
+++ lua/coq_3p/copilot/init.lua
@@ -8,7 +8,6 @@ return function(spec)
 
   vim.g.copilot_no_tab_map = true
   vim.g.copilot_assume_mapped = true
-  vim.g.copilot_tab_fallback = ""
   vim.api.nvim_set_keymap(
     "i",
     tmp_accept_key,
