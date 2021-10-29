4c425be9ffbe35360781d9a36568321ce7c48eef
root <root@dev.lan>
2021-10-28 17:22:30 -0700

    what

--- lua/coq_3p/copilot/init.lua
+++ lua/coq_3p/copilot/init.lua
@@ -2,9 +2,14 @@ return function(spec)
   vim.g.copilot_no_tab_map = true
   vim.g.copilot_assume_mapped = true
   vim.g.copilot_tab_fallback = ""
+  vim.api.nvim_set_keymap(
+    "i",
+    "<a-l>",
+    [[copilot#Accept()]],
+    {nowait = true, silent = true, expr = true}
+  )
 
   return function(args, callback)
     callback(nil)
   end
-  -- count to 20
 end
