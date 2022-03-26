de30a6aa95eb52ef04b0d474365c3114fa85a078
IIIIII <github@bigly.dog>
2022-03-26 13:08:27 -0700

    30

--- lua/coq_3p/copilot/init.lua
+++ lua/coq_3p/copilot/init.lua
@@ -24,87 +24,116 @@ return function(spec)
     {nowait = true, silent = true, expr = true}
   )
 
+  local maybe_item = function(row, col, suggestion)
+    vim.validate {
+      row = {row, "number"},
+      col = {col, "number"},
+      suggestion = {suggestion, "table"}
+    }
+    vim.validate {
+      position = {suggestion.position, "table"},
+      label = {suggestion.displayText, "string"},
+      new_text = {suggestion.text, "string"},
+      range = {suggestion.range, "table"}
+    }
+    local cop_row, cop_col =
+      suggestion.position.line,
+      suggestion.position.character
+    vim.validate {cop_row = {cop_row, "number"}, cop_col = {cop_col, "number"}}
+
+    local same_row = cop_row == row
+    local col_diff = col - cop_col
+    local almost_same_col = col_diff >= 0 and col_diff < 6
+
+    if not (same_row and almost_same_col) then
+      return nil
+    else
+      local filterText = vim.fn.matchstr(suggestion.text, [[\v^\S+]])
+
+      local range =
+        (function()
+        local fin = suggestion.range["end"]
+
+        vim.validate {
+          start = {suggestion.range.start, "table"},
+          ["end"] = {fin, "table"}
+        }
+        vim.validate {
+          line = {fin.line, "number"},
+          character = {fin.character, "number"}
+        }
+
+        local ending =
+          (function()
+          if fin.line ~= row then
+            return fin
+          else
+            return {
+              line = fin.line,
+              character = fin.character + col_diff
+            }
+          end
+        end)()
+
+        return {
+          start = suggestion.range.start,
+          ["end"] = ending
+        }
+      end)()
+
+      local item = {
+        preselect = true,
+        label = suggestion.displayText,
+        insertText = suggestion.text,
+        filterText = filterText,
+        documentation = suggestion.displayText,
+        textEdit = {
+          newText = suggestion.text,
+          range = range
+        }
+      }
+      return item
+    end
+  end
+
+  local items = (function()
+    local ooda = nil
+    local suggestions = {}
+    ooda = function()
+      local copilot = vim.b._copilot
+      if copilot then
+        vim.validate {copilot = {copilot, "table"}}
+        suggestions = copilot.suggestions or {}
+        vim.validate {suggestions = {suggestions, "table"}}
+      end
+      vim.defer_fn(ooda, 88)
+    end
+    ooda()
+
+    return function(row, col)
+      local items = {}
+      for _, suggestion in pairs(suggestions) do
+        local item = maybe_item(row, col, suggestion)
+        if item then
+          table.insert(items, item)
+        end
+      end
+      return items
+    end
+  end)()
+
   return function(args, callback)
     local row, col = unpack(args.pos)
+    local _, u16_col = vim.str_utfindex(args.line, col)
     local _ = nil
 
     (function()
-      local suggestion = vim.b._copilot_suggestion
-      if not (type(suggestion) == "table" and type(suggestion.text) == "string") then
-        callback(nil)
-      else
-        vim.validate {
-          position = {suggestion.position, "table"},
-          label = {suggestion.displayText, "string"},
-          new_text = {suggestion.text, "string"},
-          range = {suggestion.range, "table"}
+      callback(
+        {
+          isIncomplete = true,
+          items = items(row, u16_col)
         }
-
-        local _, u16_col = vim.str_utfindex(args.line, col)
-        local cop_row, cop_col =
-          suggestion.position.line,
-          suggestion.position.character
-        vim.validate {row = {row, "number"}, col = {col, "number"}}
-
-        local same_row = cop_row == row
-        local col_diff = u16_col - cop_col
-        local almost_same_col = col_diff >= 0 and col_diff < 6
-
-        if not (same_row and almost_same_col) then
-          callback(nil)
-        else
-          local filterText = vim.fn.matchstr(suggestion.text, [[\v^\S+]])
-
-          local range =
-            (function()
-            local fin = suggestion.range["end"]
-
-            vim.validate {
-              start = {suggestion.range.start, "table"},
-              ["end"] = {fin, "table"}
-            }
-            vim.validate {
-              line = {fin.line, "number"},
-              character = {fin.character, "number"}
-            }
-
-            local ending =
-              (function()
-              if fin.line ~= row then
-                return fin
-              else
-                return {
-                  line = fin.line,
-                  character = fin.character + col_diff
-                }
-              end
-            end)()
-
-            return {
-              start = suggestion.range.start,
-              ["end"] = ending
-            }
-          end)()
-
-          local item = {
-            preselect = true,
-            label = suggestion.displayText,
-            insertText = suggestion.text,
-            filterText = filterText,
-            documentation = suggestion.displayText,
-            textEdit = {
-              newText = suggestion.text,
-              range = range
-            }
-          }
-          callback(
-            {
-              isIncomplete = true,
-              items = {item}
-            }
-          )
-        end
-      end
+      )
     end)()
   end
 end
